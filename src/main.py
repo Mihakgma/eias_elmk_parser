eac834f326b4dd9a010600c439f2d99cc68e35a7
Tabakaev Mihail <mihakgma86@hotmail.com>
2024-11-11 15:51:27 +0700

    fix in main.py file about adding parameters

--- src/main.py
+++ src/main.py
@@ -7,12 +7,16 @@ class ELMKParser:
     def start(text_caching_sleep=3,
               ask_for_cancel_interval_navigator=5000,
               sleep_secs_up_to_navigator=1.1,
-              sleep_secs_up_to_pesr_data_navigator=0.5):
+              sleep_secs_up_to_pesr_data_navigator=0.5,
+              max_iterations_caching=15,
+              cache_dir_caching="\\text_data"):
         session_manager = SessionManager()
         session_manager.start_new_session()
         navigator = session_manager.get_navigator()
         text_caching = TextCaching(sleep_time=text_caching_sleep,
-                                   obj=navigator)
+                                   obj=navigator,
+                                   cache_dir=cache_dir_caching,
+                                   max_iterations=max_iterations_caching)
         text_caching.start()
         navigator(ask_for_cancel_interval=ask_for_cancel_interval_navigator,
                   sleep_secs_up_to=sleep_secs_up_to_navigator,
