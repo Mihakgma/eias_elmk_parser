94e894f1eb9da080cfcd88bdcfbdb8c7a6e16e44
Tabakaev Mihail <mihakgma86@hotmail.com>
2024-11-21 11:35:21 +0700

    in browser_error_wrapper.py file added exception traceback & wrapped function name printing...

--- src/patterns/browser_error_wrapper.py
+++ src/patterns/browser_error_wrapper.py
@@ -6,28 +6,46 @@ from classes.driver import Driver
 
 def handle_exceptions_quit_driver(func):
     """Decorator for browser exceptions handling"""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         last_word = "Press any key to quit webdriver..."
         try:
             return func(*args, **kwargs)
         except NoSuchWindowException as e:
-            __print_error_discharge_driver(e, last_word)
+            __print_error_discharge_driver(exception=e,
+                                           function=func,
+                                           text=last_word)
             return None
         except WebDriverException as wde:
-            __print_error_discharge_driver(wde, last_word)
+            __print_error_discharge_driver(exception=wde,
+                                           function=func,
+                                           text=last_word)
             return None
         except TypeError as te:
-            __print_error_discharge_driver(te, last_word)
+            __print_error_discharge_driver(exception=te,
+                                           function=func,
+                                           text=last_word)
             return None
         except BaseException as be:
-            __print_error_discharge_driver(be, last_word)
+            __print_error_discharge_driver(exception=be,
+                                           function=func,
+                                           text=last_word)
             return None
 
     return wrapper
 
+
 def __print_error_discharge_driver(exception,
-                                 text: str = ""):
+                                   function,
+                                   text: str = "",
+                                   traceback: bool = True):
     print(f"An error | exception occurred: {exception}")
+    print(f"<{function.__name__}> function has been crashed on ...")
+    if traceback:
+        tb_lines = traceback.extract_stack()
+        for line in tb_lines:
+            if function.__name__ in line.filename:
+                print(f" {line.filename}:{line.lineno} in {line.funcName} ({line.line})")
     input(text)
     Driver.CURRENT_INSTANCE.discharge()
