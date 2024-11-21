127719fb30afd98984c0fabd151e9d542a741617
Tabakaev Mihail <mihakgma86@hotmail.com>
2024-11-21 11:17:15 +0700

    in handle_exceptions_quit_driver() function-wrapper added case for BaseException to quit web-driver anyway... it is essential for my application, also added __print_error_discharge_driver() to process while errors occurred...

--- src/patterns/browser_error_wrapper.py
+++ src/patterns/browser_error_wrapper.py
@@ -8,19 +8,26 @@ def handle_exceptions_quit_driver(func):
     """Decorator for browser exceptions handling"""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        last_word = "Press any key to quit webdriver..."
         try:
             return func(*args, **kwargs)
         except NoSuchWindowException as e:
-            print(f"Произошла ошибка: {e}")
-            Driver.CURRENT_INSTANCE.discharge()
+            __print_error_discharge_driver(e, last_word)
             return None
-        except WebDriverException as e:
-            print(f"Произошла ошибка: {e}")
-            Driver.CURRENT_INSTANCE.discharge()
+        except WebDriverException as wde:
+            __print_error_discharge_driver(wde, last_word)
             return None
         except TypeError as te:
-            print(f"Произошла ошибка: {te}")
-            Driver.CURRENT_INSTANCE.discharge()
+            __print_error_discharge_driver(te, last_word)
+            return None
+        except BaseException as be:
+            __print_error_discharge_driver(be, last_word)
             return None
 
     return wrapper
+
+def __print_error_discharge_driver(exception,
+                                 text: str = ""):
+    print(f"An error | exception occurred: {exception}")
+    input(text)
+    Driver.CURRENT_INSTANCE.discharge()
