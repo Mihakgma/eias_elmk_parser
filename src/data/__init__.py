f54710592e0d01c4ffce8507b78405780a1721fc
Tabakaev Mihail <mihakgma86@hotmail.com>
2024-11-22 14:09:28 +0700

    import issues f....ck.... AGAIN ...OMFFFFFFFFFFFG!!! FIIIDSJIDSUUUUUUUUUUUUUUUUUUUKKKKKKKKKKKKKKKKKKKKK!!!

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,5 @@
 from data.variables import *  # Лучше всего здесь импортировать только необходимые переменные, а не все *
-from data.constants import *
+# from data.constants import *
 
 
 if __name__ == "__main__":
--- src/functions/parsing_functions.py
+++ src/functions/parsing_functions.py
@@ -15,9 +15,42 @@ from pyautogui import hotkey as pyt_hotkey
 from pyautogui import click as pyt_click
 from pyautogui import locateOnScreen as pyt_locateOnScreen
 
+from classes import driver
 from patterns.thread_func import thread
 from data import *
 
+
+def get_constants():
+    COLNAMES_DICT = (
+        WAIT_DICT,
+        REGISTR_ADRESS_DICT,
+        FACT_ADRESS_DICT,
+        GENDER_DICT,
+        TEL_NUMB_DICT,
+        LAST_NAME_DICT,
+        FIRST_NAME_DICT,
+        MIDDLE_NAME_DICT,
+        EMAIL_DICT,
+        PASSPRT_NUMB_DICT,
+        PASSPRT_DATE_DICT,
+        PASSPRT_DEPT_DICT,
+        PASSPRT_CODE_DICT)
+
+    PERS_DATA_XPATH = {
+        WAIT_XPATH: [0, 0],
+        REGISTR_ADRESS_XPATH: [0, 0],
+        FACT_ADRESS_XPATH: [0, 1],
+        GENDER_XPATH: [1, 0],
+        TEL_NUMB_XPATH: [0, 0],
+        LAST_NAME_XPATH: [0, 0],
+        FIRST_NAME_XPATH: [0, 0],
+        MIDDLE_NAME_XPATH: [0, 0],
+        EMAIL_XPATH: [0, 0],
+        PASSPRT_NUMB_XPATH: [0, 0],
+        PASSPRT_DATE_XPATH: [0, 0],
+        PASSPRT_DEPT_XPATH: [0, 0],
+        PASSPRT_CODE_XPATH: [0, 0]}
+    return COLNAMES_DICT, PERS_DATA_XPATH
 
 @thread
 def submit_certificate(cert_screen_files_path: list = "",
@@ -443,7 +476,8 @@ def get_filial_name(first_lst_in: list,
 def get_personal_data(driver,
                       sleep_up_to: float,
                       in_new_window: bool = False):
-    COLNAMES_DICT, PERS_DATA_XPATH = Constant.get_constants()
+    COLNAMES_DICT, PERS_DATA_XPATH = get_constants()
+    print(COLNAMES_DICT, PERS_DATA_XPATH)
     # in new window and close it after parsing is ended
     if in_new_window:
         if len(list(driver.window_handles)) > 1:
@@ -541,4 +575,7 @@ def click_element_by_xpath(driver, xpath, timeout=15, in_new_window: bool = Fals
 
 
 if __name__ == '__main__':
-    print(COLNAMES_DICT, PERS_DATA_XPATH)
+    from classes.driver import Driver
+    # COLNAMES_DICT, PERS_DATA_XPATH = get_constants()
+    # print(COLNAMES_DICT, PERS_DATA_XPATH)
+    get_personal_data(driver=Driver(), sleep_up_to=5, in_new_window=True)
